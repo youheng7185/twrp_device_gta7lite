de79aaf538c8d77c1eb0440f0e4fdb16a118510f
Your Name <you@example.com>
2022-10-23 15:16:37 +0800

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_gta7lite.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_gta7lite-user \
+    twrp_gta7lite-userdebug \
+    twrp_gta7lite-eng
